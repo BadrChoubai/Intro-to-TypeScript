5a7db7912a7c18a53ec970b11a9fe36e11bdea49
Badr Choubai <badrchoubai@outlook.com>
2020-05-24 01:37:45 -0600

    Component class

---
+++ src/components/Component.ts
@@ -0,0 +1,24 @@
+/*eslint no-empty-function: ["error", { "allow": ["functions", "methods"] }]*/
+
+import Store from '../store/index.js';
+
+export default class Component {
+    render: any;
+    element: any;
+
+    constructor(props: { [key: string]: any } = {}) {
+        this.render =
+            this.render ||
+            function () {
+                return;
+            };
+
+        if (props.store instanceof Store) {
+            props.store.events.subscribe('stateChange', () => this.render()); // https://stackoverflow.com/questions/51439843/unknown-vs-any
+        }
+
+        if (props.hasOwnProperty('element')) {
+            this.element = props.element;
+        }
+    }
+}
